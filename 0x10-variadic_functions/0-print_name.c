673ea789d54ee9acafc12d9cb68644af8faf42c4
jimano1 <jimano.ads@gmail.com>
2023-04-20 20:08:57 -0700

    test

--- 0x10-variadic_functions/0-print_name.c
+++ 0x10-variadic_functions/0-print_name.c
@@ -1,16 +1,29 @@
-#include "function_pointers.h"
-#include <stdio.h>
-/**
- * print_name - print name using pointer to function
- * @name: a string representing the name to be printed.
- * @f: pointer to function
- *
- * Return: nothing
- **/
-void print_name(char *name, void (*f)(char *))
-{
-	if (name == NULL || f == NULL)
-		return;
+#include "variadic_functions.h"
+#include <stdarg.h>
 
-	f(name);
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: the number of parameters passed to the function.
+ * @...: a variable number of parameters to calculate the sum of.
+ *
+ * Return: If n == 0 - 0.
+ *         Otherwise - the sum of all parameters.
+ **/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
 }
