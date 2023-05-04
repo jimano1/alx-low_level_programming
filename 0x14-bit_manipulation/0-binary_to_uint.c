e8397137512733675989c7d4a95c7ace6ca1decc
jimano1 <jimano.ads@gmail.com>
2023-05-04 16:11:23 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 
 	if (!b)
-		return 0;
+		return (0);
 
 	for (int i = 0; b[i] != '\0'; ++i)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			return 0;
+			return (0);
 		num <<= 1;
 		if (b[i] == '1')
 			num += 1;
 	}
 
-	return num;
+	return (num);
 }
