8ae7b349683515ba30aa4bc5481a696e7cb04b99
jimano1 <jimano.ads@gmail.com>
2023-05-09 01:20:31 +0000

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,35 @@
 #include "file.h"
-#include <stdio.h>
 
 /**
- * create_file - Create a new file and write text to it.
- * @filename: The name of the file to create.
- * @text_content: The text to write to the file.
- *
- * Return: On success, 1. On failure, -1.
+ * create_file - create a file
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: a data
+ * Return: Success ? 1 : -1
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t num_bytes_written;
+	int file_desc, _write;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
+	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_desc == -1)
 		return (-1);
 
-	if (text_content)
+	if (!text_content)
 	{
-		num_bytes_written = write(fd, text_content, strlen(text_content));
-		if (num_bytes_written == -1 || num_bytes_written != strlen(text_content))
-		{
-			close(fd);
-			return (-1);
-		}
+		close(file_desc);
+		return (1);
 	}
 
-	close(fd);
+	_write = write(file_desc, text_content, slen(text_content));
+	if (_write == -1 || _write != slen(text_content))
+	{
+		close(file_desc);
+		return (-1);
+	}
+	close(file_desc);
 	return (1);
 }
